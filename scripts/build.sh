2e4f44e291a4d53e1fc64e28fd2e07d0ba3484e1
darthjee <darthjee@gmail.com>
2020-04-23 10:37:55 +0200

    Avoid spill

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ FILES="cyberhawk.js controller.js notifier.js requester.js"
 echo "" > cyberhawk.js
 
 for FILE in $FILES; do
-  cat src/$FILE >> cyberhawk.js
+  cat "src/$FILE" >> cyberhawk.js
   echo "" >> cyberhawk.js
 done
