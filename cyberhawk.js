1fd4dc2a9ceef6145e280de4ae24ac64cd88355a
darthjee <darthjee@gmail.com>
2021-08-12 20:24:37 -0300

    Convert notifier

--- cyberhawk.js
+++ cyberhawk.js
@@ -198,28 +198,28 @@
 (function(_, angular, Cyberhawk) {
   var module = angular.module("cyberhawk/notifier", []);
 
-  function NotifierService() {
-    this.watchs = {};
-  }
-
-  var fn = NotifierService.prototype;
-
-  fn.notify = function(key, value) {
-    _.each(this.listeners(key), function(callback) {
-      callback(value);
-    });
-  };
-
-  fn.register = function(key, callback) {
-    this.listeners(key).push(callback);
-  };
-
-  fn.listeners = function(key) {
-    if (typeof this.watchs[key] === "undefined") {
-      this.watchs[key] = [];
+  class NotifierService {
+    constructor() {
+      this.watchs = {};
     }
-    return this.watchs[key];
-  };
+
+    notify(key, value) {
+      _.each(this.listeners(key), function(callback) {
+        callback(value);
+      });
+    }
+
+    register(key, callback) {
+      this.listeners(key).push(callback);
+    }
+
+    listeners(key) {
+      if (typeof this.watchs[key] === "undefined") {
+        this.watchs[key] = [];
+      }
+      return this.watchs[key];
+    }
+  }
 
   Cyberhawk.NotifierServiceFactory = function() {
     return new NotifierService();
