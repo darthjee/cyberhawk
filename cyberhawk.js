c868e1b5e4903851469190c4a752089365a5556c
darthjee <darthjee@gmail.com>
2024-05-02 13:26:29 -0300

    Fix controller build

--- cyberhawk.js
+++ cyberhawk.js
@@ -705,27 +705,36 @@
 
   class ControllerBuilderService {
     constructor(requesterBuilder, notifier, $location, $timeout, pagination, route) {
-      this.attributes = {
-        requester: requesterBuilder.build($location),
-        notifier: notifier,
-        pagination: pagination,
-        location: $location,
-        $timeout: $timeout,
-        routeParams: route.current.pathParams,
-        route: route.current.$$route.route
-      }
+      this.requesterBuilder = requesterBuilder;
+      this.notifier = notifier;
+      this.pagination = pagination;
+      this.$location = $location;
+      this.$timeout = $timeout;
+      this.route = route;
     }
 
     build(controller) {
       _.extend(controller.constructor.prototype, Controller.prototype);
 
-      _.extend(controller, this.attributes);
+      _.extend(controller, this.attributes());
       Controller.extend(controller.route, controller);
       _.bindAll(controller, "_setData", "save", "request", "_goIndex", "_error");
       controller.requester.bind(controller);
 
 
       controller.request();
+    }
+
+    attributes() {
+      return {
+        requester: this.requesterBuilder.build(this.$location),
+        notifier: this.notifier,
+        pagination: this.pagination,
+        location: this.$location,
+        $timeout: this.$timeout,
+        routeParams: this.route.current.pathParams,
+        route: this.route.current.$$route.route
+      }
     }
   }
 
