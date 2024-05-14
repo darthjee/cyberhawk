e813344996c20ca0ea941b0699b761f76f0e152b
darthjee <darthjee@gmail.com>
2024-05-14 18:24:43 -0300

    Remove duplication

--- src/controller.js
+++ src/controller.js
@@ -7,45 +7,7 @@
     app = angular.module("cyberhawk/controller", [
       "cyberhawk/notifier", "cyberhawk/requester",
       "cyberhawk/pagination"
-    ]),
-
-    ExtensionMethods = {
-      withPath: function(path, name, func) {
-        if (path.constructor == Array) {
-          let klass = this;
-
-          return _.each(path, function(route) {
-            klass.withPath(route, name, func);
-          });
-        }
-
-        if (!this.pathExtensions[path]) {
-          this.pathExtensions[path] = {};
-        }
-
-        if (typeof name == "string") {
-          this.pathExtensions[path][name] = func
-        } else {
-          _.extend(this.pathExtensions[path], name);
-        }
-      },
-
-      extensionFor: function(path) {
-        return this.pathExtensions[path] || {};
-      },
-
-      extend: function(path, controller) {
-        var methods = this.extensionFor(path);
-
-        _.extend(controller, methods);
-
-        for(method in methods) {
-          _.bindAll(controller, method)
-        }
-      },
-
-      pathExtensions: {}
-    };
+    ]);
 
   _.extend(Controller, HooksMethods, ExtensionMethods);
 
