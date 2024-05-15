4aac119a74d32a3c153be240ba1605c6a8913ae7
darthjee <darthjee@gmail.com>
2024-05-15 14:33:50 -0300

    Regactor hooks

--- src/hooks_methods.js
+++ src/hooks_methods.js
@@ -4,46 +4,49 @@
       if (path.constructor == Array) {
         let klass = this;
 
-        return _.each(path, function(route) {
-          klass.on(route, event, func);
+        _.each(path, function(route) {
+          klass.setPathHook(route, event, func);
         });
+      } else {
+        setPathHook(path, event, func);
       }
-
-      if (!this.pathHooks[path]) {
-        this.pathHooks[path] = {};
-      }
-
-      if (!this.getPathHooks(path, event)) {
-        this.getPathHooks(path, event) = [];
-      }
-
-      this.getPathHooks(path, event).push(func);
-    },
-
-    pathHooksFor(path, event) {
-      if (!this.pathHooks[path]) {
-        return [];
-      }
-
-      if (!this.getPathHooks(path, event)) {
-        return [];
-      }
-
-      return this.getPathHooks(path, event);
     },
 
     trigger(controller, path, event) {
-      var hooks = this.pathHooksFor(path, event);
+      var hooks = this.getPathHooks(path, event);
 
-      _.each(hooks, function(func) {
-        if (typeof func == "string") {
-          controller[func]();
-        } else {
-          func.apply(controller);
-        }
-      });
+      if ( hooks ) {
+        _.each(hooks, function(func) {
+          if (typeof func == "string") {
+            controller[func]();
+          } else {
+            func.apply(controller);
+          }
+        });
+      }
     },
 
-    pathHooks: {}
+    setPathHook(path, event, func) {
+      hooks = getPathHooks(path, event);
+
+      if (!hooks) {
+        if ( !this.pathHooks[path] ){
+          this.pathHooks[path] = {};
+        }
+        hooks = this.pathHooks[path][event] = [];
+      }
+
+      hooks.push(func);
+    },
+
+    getPathHooks(path, event) {
+      if (!this.pathHooks) {
+        this.pathHooks = {};
+      }
+
+      if (this.pathHooks[path]) {
+        return this.pathHooks[path][event];
+      }
+    }
   };
 }(window._, local));
