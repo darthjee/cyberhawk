6d53f2e77f40f45fdc2a277a5a3a5e8b27bf72cd
darthjee <darthjee@gmail.com>
2025-05-12 19:33:59 -0300

    Callback comes at the end

--- cyberhawk.js
+++ cyberhawk.js
@@ -756,12 +756,12 @@
     build() {
       if (!this._isBuilt()) {
         this._addMethods();
-        this._callback();
       }
 
       _.extend(this.controller, this.attributes);
 
       this._bind();
+      this._callback();
     }
 
     _callback() {
