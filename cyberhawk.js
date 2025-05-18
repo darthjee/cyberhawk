cf5b878d0a289e6e048c1e99bdc865ea60d196bc
darthjee <darthjee@gmail.com>
2025-05-18 16:36:23 -0300

    Add global state

--- cyberhawk.js
+++ cyberhawk.js
@@ -871,5 +871,23 @@
 }(window._, window.angular, local));
 
 // global_state.js
+(function(_, angular) {
+  var module = angular.module("cyberhawk/global_state", []);
+
+
+  class GlobalStateService {
+    constructor() {
+      this.id = Math.floor(Math.random() * 1000000);
+    }
+  }
+
+  function GlobalStateServiceFactory($http) {
+    return new GlobalStateService($http);
+  }
+
+  module.service("global_state", [
+    GlobalStateServiceFactory
+  ]);
+}(window._, window.angular));  
 
 }());
